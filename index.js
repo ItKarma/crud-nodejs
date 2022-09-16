58223cce19c2e85ecc353af46b43b074791982bc
sheynz <loppesofc@gmail.com>
2022-09-16 09:36:56 -0300

    import from db

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const routers = require('./router');
 const app = express();
 
+require('./config/db')
+
 app.use(express.json())
 app.use('/', routers)
 
 app.listen(3000, ()=>{
   console.log('Servidor ligado !!.')
 })
-
-
-
