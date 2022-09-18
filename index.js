1da9f3fc0cacb1c5af8797f2e6e4cb45b7d003f2
sheynz <loppesofc@gmail.com>
2022-09-18 14:53:08 -0300

    Configurando template engine

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path')
+const logger = require('morgan')
 const routers = require('./router');
 const app = express();
 
 require('./config/db')
 
+app.use(logger('dev'))
 app.use(express.json())
+
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
 app.use('/', routers)
 
 app.listen(3000, ()=>{
