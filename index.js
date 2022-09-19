032fbde43ad6dc7ab2a17f8e9f0a07f5ba5c2939
sheynz <loppesofc@gmail.com>
2022-09-19 11:20:54 -0300

    my crudjs

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 
 app.use('/', routerPages)
-app.use('/checklist', routerPages)
+app.use('/checklist', routerList)
 
 app.listen(3000, ()=>{
   console.log('Servidor ligado !!.')
